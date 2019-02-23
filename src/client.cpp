15ff3c020cd31f72c0703c773274559941e3615d
Zhiwen Hu <zhiwenhu@Zhiwens-MacBook-Pro-2.local>
2019-02-23 11:26:20 -0800

    udp template with file transmission

--- src/client.cpp
+++ src/client.cpp
@@ -1,8 +1,96 @@
-#include <string>
-#include <thread>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
-int main()
-{
-  std::cerr << "client is not implemented yet" << std::endl;
+//http://developerweb.net/viewtopic.php?id=3196
+
+using namespace std;
+#define size 1024
+struct addrinfo hints, *infoptr;
+
+int main(int argc, char* argv[]){
+
+  if(argc != 4){
+    cerr << "ERROR: Number of arguments incorrect. Try ./client <HOSTNAME/IP> <PORT> <FILENAME>\n";
+    exit(EXIT_FAILURE);
+  }
+
+  char* host_name = argv[1];
+  int port = atoi(argv[2]);
+  char* file_name = argv[3];
+
+
+  if(port < 1024 || port > 65535){
+    cerr << "ERROR: Port numbers incorrect. Try another one.\n";
+    exit(EXIT_FAILURE);
+  }
+
+
+  int clientSocket, portNum, nBytes;
+  char buffer[1024];
+  struct sockaddr_in serverAddr;
+  socklen_t addr_size;
+
+  /*Create UDP socket*/
+  clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
+
+  /*Configure settings in address struct*/
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(port);
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+
+  /*Initialize size variable to be used later on*/
+  addr_size = sizeof serverAddr;
+
+  ifstream input (file_name,  ios::binary);
+  if(!input.is_open()){
+      cerr << "ERROR: cannot open the file" << endl;
+      exit(EXIT_FAILURE);
+  }
+
+
+
+  while(1){
+    memset(buffer, '\0', sizeof(buffer));
+    int bytes_send = input.read(buffer, sizeof(buffer)).gcount();
+
+    if (sendto(clientSocket,buffer,bytes_send,0,(struct sockaddr *)&serverAddr,addr_size) < 0) {
+        perror("send to");
+        exit(EXIT_FAILURE);
+    }
+
+    if (bytes_send == 0){
+        cout << "done with sending file" << endl;
+        break;
+    }
+
+
+    /*Receive message from server*/
+//                nBytes = recvfrom(clientSocket,buffer,1024,0,NULL, NULL);
+//    printf("Received from server: %s\n",buffer);
+
+  }
+
+
+  input.close();
+  close(clientSocket);
+
+  return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,8 +1,119 @@
-#include <string>
-#include <thread>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <signal.h>
+#include <fcntl.h>
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <thread>
+#include <cstring>
+using namespace std;
 
-int main()
+#define size 4096
+int thread_id = 0;
+static const int num_threads = 11;
+
+
+void sig_quit_handler(int s)
 {
-  std::cerr << "server is not implemented yet" << std::endl;
+  cout << "Interrupt signal (SIGQUIT) received.\n";
+  exit(0);
+}
+
+void sig_term_handler(int s)
+{
+  cout << "Interrupt signal (SIGTERM) received.\n";
+  exit(0);
+}
+
+
+
+int main(int argc, char* argv[]){
+
+    signal(SIGQUIT, sig_quit_handler);
+    signal(SIGTERM, sig_term_handler);
+
+    if (argc != 3) {
+        cerr << "ERROR: Number of arguments incorrect. Try ./server <PORT> <FILE-DIR>\n";
+        exit(EXIT_FAILURE);
+    }
+
+    int port = atoi(argv[1]);
+    char *save_directory = argv[2];
+
+    struct stat st = {0};
+    if (stat(save_directory, &st) == -1) {
+        mkdir(save_directory, 0700);
+    }
+
+    if (port < 1024 || port > 65535) {
+        cerr << "ERROR: Port numbers incorrect. Try another one.\n";
+        exit(EXIT_FAILURE);
+    }
+
+
+
+  int udpSocket, nBytes;
+  char buffer[1024];
+  struct sockaddr_in serverAddr, clientAddr;
+  struct sockaddr_storage serverStorage;
+  socklen_t addr_size, client_addr_size;
+
+  /*Create UDP socket*/
+  udpSocket = socket(PF_INET, SOCK_DGRAM, 0);
+
+  /*Configure settings in address struct*/
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(port);
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+
+  /*Bind socket with address struct*/
+  bind(udpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+  /*Initialize size variable to be used later on*/
+  addr_size = sizeof serverStorage;
+
+
+// save to file
+  char file_name[size];
+  sprintf(file_name,"%s/%d.file", save_directory, 1);
+
+  ofstream output(file_name, ios::out | ios::trunc | ios::binary);
+
+
+  while(1){
+    memset(buffer, '\0', sizeof(buffer));
+    nBytes = recvfrom(udpSocket,buffer,1024,0,(struct sockaddr *)&serverStorage, &addr_size);
+
+    cout << buffer << endl;
+
+    if (nBytes == -1){
+        cerr << "ERROR: byte receive error";
+        output.close();
+        close(udpSocket);
+    }
+
+    if (nBytes == 0){
+        break;
+    }
+
+    output.write(buffer, nBytes);
+    printf("server received %d bytes\n", nBytes);
+
+//    sendto(udpSocket,buffer,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
+  }
+
+//  output.close();
+//  close(udpSocket);
+  return 0;
 }
